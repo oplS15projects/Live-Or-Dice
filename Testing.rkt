b1ea21544ecfe27874e04937988ba971aab7ca22
csopiep <csopiep@gmail.com>
2015-04-22 10:16:33 -0400

    test

--- Testing.rkt
+++ Testing.rkt
@@ -1,20 +1,8 @@
 #lang racket
-(require racket/gui/base)
 
+(require racket/gui)
 
-(define mainframe (new frame% [label "Live or Dice"]
-                              [alignment '(center bottom)]
-                              ))
-
-(define mess3 (new message% [parent mainframe]
-                   [label "0          "]
-                   ))                
-
-(define bpanel (new horizontal-panel% [parent mainframe]
-                       [alignment '(center center)]
-                      [spacing 15]
-                      ))
-
+;roll function
 (define b 0)
 (define (dice) (+ 1 (random 6)))
 (define sum  (lambda ()  (set! b (+ b (dice)))))
@@ -25,18 +13,40 @@
 
 
 (define (roll) (sum) (conf b))
+;Frame Main Window
+; Make a frame by instantiating the frame% class
+(define frame (new frame%
+                   [label "Example"]
+                   [width 300]
+                   [height 300]))
+ 
+; Make a static text message in the frame
+(define msg (new message% [parent frame]
+                          [label "No events so far..."]))
+ 
+; Make a button in the frame
+;(new button% [parent frame]
+;             [label "Click Me"]
+             ; Callback procedure for a button click:
+;             [callback (lambda (button event)
+;                         (send msg set-label "Button click"))])
+ 
+; Show the frame by calling its show method
+(send frame show #t)
 
+;Canvas for Main Window
+(new canvas% [parent frame]
+             [paint-callback
+              (lambda (canvas dc)
+                (send dc set-scale 3 3)
+                (send dc set-text-foreground "blue")
+                (send dc draw-text "ROLL IT!" 0 0))])
 
-(new button% [parent bpanel]
-     [label "Roll"]
-  [callback (lambda (button event) (send mess3 set-label (number->string  (roll))))]
-      )
+(send frame show #t)
 
-
-(new button% [parent bpanel] [label "Pass"]
-
-)
-
-
-
-(send mainframe show #t)
+(define panel (new horizontal-panel% [parent frame]
+                   [alignment '(center center)]))
+(new button% [parent panel]
+             [label "Roll"]
+             [callback (lambda (button event)
+                         (send msg set-label (number->string (roll))))])
